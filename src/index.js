760271123052e17cac0d6e34bb12475fd97d7282
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-05-06 00:22:23 +0300

    start 1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import Notiflix from "notiflix";
 import simpleLightbox from "simplelightbox";
 
+form = document.querySelector(".search-form");
+gallery = document.querySelector(".gallery");
+btnLoadMore = document.querySelector(".load-more");
+
+
+
 // function getPictures(pictureName, page) {
 //   const URL = `https://pixabay.com/api/?key=35723548-55cce6d92fe2b0376e8aa06a2&q="${pictureName}"
-//     &image_type="photo"&orientation="horizontal"&safesearch="true"&per_page=200&page=${page}`;
+//     &image_type="photo"&orientation="horizontal"&safesearch="true"&per_page=40&page=${page}`;
 //   return fetch(URL).then((resp) => {
 //     if (!resp.ok) {
 //       throw new Error(resp.statusText);
@@ -27,10 +33,22 @@ import simpleLightbox from "simplelightbox";
 //     })
 // }
 
-// function createCommentMarkup({ title, body, id }) {
-//   return `<li data-id="${id}">
-//     <h2>${title}</h2>
-//     <p>${body}</p>
-//     <button>Видалити відгук</button>
-//     </li>`;
-// }
+function createCommentMarkup({ webformatURL, largeImageURL, tags, like, views, comments, downloads }) {
+  return `<div class="photo-card">
+  <img src="" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+    </p>
+  </div>
+</div>`;
+}
