4a849154d19322efd10063e4695148bfa0837020
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-05-06 01:40:40 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import Notiflix from "notiflix";
 import simpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import axios from 'axios';
 
 
 form = document.querySelector(".search-form");
 gallery = document.querySelector(".gallery");
 btnLoadMore = document.querySelector(".load-more");
+// const pictureName = evt.target.value.trim();
+const API_KEY = "35723548-55cce6d92fe2b0376e8aa06a2";
 
 
 
 function getPictures(pictureName, page) {
-  const URL = `https://pixabay.com/api/?key=35723548-55cce6d92fe2b0376e8aa06a2&q="${pictureName}"
+  const URL = `https://pixabay.com/api/?key=${API_KEY}&q=${pictureName}
     &image_type="photo"&orientation="horizontal"&safesearch="true"&per_page=40&page=${page}`;
   return fetch(URL).then((resp) => {
     if (!resp.ok) {
@@ -37,19 +40,19 @@ getPictures("cat", 3);
 
 function createMarkup({ webformatURL, largeImageURL, tags, like, views, comments, downloads }) {
   return (`<div class="photo-card">
-  <img src="" alt="" loading="lazy" />
+  <img src="${largeImageURL}" alt="${webformatURLtags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
-      <b>Likes</b>
+      <b>Likes</b>:${like}
     </p>
     <p class="info-item">
-      <b>Views</b>
+      <b>Views</b>:${views}
     </p>
     <p class="info-item">
-      <b>Comments</b>
+      <b>Comments</b>:${comments}
     </p>
     <p class="info-item">
-      <b>Downloads</b>
+      <b>Downloads</b>:${downloads}
     </p>
   </div>
 </div>`)
