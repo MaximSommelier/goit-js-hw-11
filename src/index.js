7355d05540e104a661303dd1f03b1db1fd56c029
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-05-07 23:47:43 +0300

    semi

--- src/index.js
+++ src/index.js
@@ -4,33 +4,42 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import axios from 'axios';
 
 
-form = document.querySelector(".search-form");
+form = document.querySelector("#search-form");
 gallery = document.querySelector(".gallery");
 btnLoadMore = document.querySelector(".load-more");
+
+btnLoadMore.addEventListener('click',onBtnLoadMore);
+form.addEventListener('submit',onSearch);
 
 let page = 1;
 let counter = 0;
 
-form.addEventListener('submit',onSearch);
-
 function onSearch(evt){
 evt.preventDefault();
-gallery.insertAdjacementHTML = "";
-const pictureName = evt.target.value; 
+gallery.innerHTML = "";
+const pictureName = evt.target.elements.searchQuery.value.trim(); 
 
-const picture = getFetch(pictureName)
-createMarkup(picture)
+ if (pictureName === "" || !pictureName){
+  return
+ }
+console.log(pictureName);
+const pictures = getFetch(pictureName);
+
+getFetch(pictureName).then(pictures => createMarkup(pictures));
+console.log(pictures);
 btnLoadMore.hidden = false;
 }
 
-function onBtnLoadMore(){
-  btnloadMore.addEventListener('click',onClick);
+function onBtnLoadMore(evt){
   counter +=1;
-
-  if (counter !== resp.total)
-  {page += 1;
-  const picture = getFetch(pictureName)
-  createMarkup(picture)}
+  page += 1;
+  const pictureName = evt.target.elements.searchQuery.value.trim(); 
+  const pictures = getFetch(pictureName);
+  getFetch(pictureName).then(pictures => createMarkup(pictures));
+  // if (counter !== resp.total)
+  // {page += 1;
+  // getFetch(pictureName).then(pictures => createMarkup(pictures))
+  // }
 
   btnLoadMore.hidden = true;
   Notiflix.Notify.info("We're sorry, but you've reached the end of search results.")
@@ -40,14 +49,14 @@ function onBtnLoadMore(){
 function createMarkup(data) {
   const markup = data.hits.map(
     ({
-    webformatURLtags,
+    webformatURL,
     tags,   
     likes,
     views,
     comments,
     downloads,
   }) => `<div class="photo-card">
-  <img src="${webformatURLtags}" alt="${tags}" loading="lazy" />
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
   <div class="info">
     <p class="info-item">
       <b>Likes</b>:${likes}
@@ -66,7 +75,8 @@ function createMarkup(data) {
 )
 .join("");
 
-gallery.insertAdjacementHTML("beforeend", markup)
+gallery.insertAdjacentHTML("beforeend", markup);
+
 if (data.hits.length < 40) {
   btnLoadMore.hidden = true;
   Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
@@ -79,23 +89,22 @@ const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = "35723548-55cce6d92fe2b0376e8aa06a2";
 const queryParams = new URLSearchParams({
   key: API_KEY,
-  q: `${pictureName}`,
+  q: pictureName,
   image_type: 'photo',
   orientation: 'horizontal',
   per_page: 40,
-  page: `${page}`,
+  page: page,
   safesearch: true,
 });
   
 try{
 const resp = await axios.get(`${BASE_URL}?${queryParams}`);
-const {data} = resp;
-return data;
+const pictures = resp.data;
+return pictures;
 
 } catch(err){
   Notiflix.Notify.info(err.message);
  }
 }
-
 
 
